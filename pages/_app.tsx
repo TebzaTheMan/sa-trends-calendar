55c901bdc1cba12023e808ce2083b9de7511ead3
Tebogo Nomnqa <tebzax2@gmail.com>
2021-12-13 23:22:34 +0200

    style: add primary color to palette

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+const theme = extendTheme({
+  colors: {
+    primary: "#70008F",
+  },
+});
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Box m={16} mt={4}>
         <Component {...pageProps} />
       </Box>
