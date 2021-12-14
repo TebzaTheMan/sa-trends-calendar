0a7cc03d703a8384b5ef73212aadc583e9eed90e
Tebogo Nomnqa <tebzax2@gmail.com>
2021-12-14 16:13:00 +0200

    feat: add popover on user avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,21 @@
-import { Heading, Image, Avatar, AvatarBadge } from "@chakra-ui/react";
+import {
+  Heading,
+  Image,
+  Avatar,
+  AvatarBadge,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  Text,
+  Badge,
+  Divider,
+  List,
+  ListItem,
+  ListIcon,
+  Link,
+} from "@chakra-ui/react";
+import { FiMessageCircle, FiPower } from "react-icons/fi";
 import { Flex, Spacer } from "@chakra-ui/react";
 
 const Header = () => {
@@ -9,9 +26,42 @@ const Header = () => {
         Trends Calendar
       </Heading>
       <Spacer />
-      <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo">
-        <AvatarBadge boxSize="1.25em" bg="primary" />
-      </Avatar>
+      <Popover placement={"bottom-end"} trigger="hover">
+        <PopoverTrigger>
+          <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo">
+            <AvatarBadge boxSize="1.25em" bg="primary" />
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent width="100">
+          <PopoverBody>
+            <Text fontSize="lg" fontWeight={"bold"}>
+              Segun Adebayo{" "}
+            </Text>
+
+            <Text fontSize="sm">@SegunAdebayo </Text>
+            <Badge variant={"solid"} bg="primary">
+              MODERATOR
+            </Badge>
+            <Divider mt={4} mb={4} />
+            <List spacing={7}>
+              <Link
+                href="https://twitter.com/messages/compose?recipient_id=4775018429&text=SA-Trends-Calendar%20Feedback:"
+                isExternal
+              >
+                <ListItem color={"gray.700"}>
+                  <ListIcon as={FiMessageCircle} color={"gray.700"} />
+                  Feedback
+                </ListItem>
+              </Link>
+
+              <ListItem color={"gray.700"}>
+                <ListIcon as={FiPower} color={"gray.700"} />
+                Logout
+              </ListItem>
+            </List>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
     </Flex>
   );
 };
