22fc02cc2ea13ba6f173b2ebf321104fb253b0b6
Tebogo Nomnqa <tebzax2@gmail.com>
2021-12-17 13:01:04 +0200

    feat: validate submitted image URLs

--- components/Month.tsx
+++ components/Month.tsx
@@ -22,9 +22,15 @@ import {
   PopoverContent,
   PopoverBody,
   Link,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { FiPlus, FiHelpCircle } from "react-icons/fi";
 import { useAuth } from "../context/auth.context";
+import { Formik, Form, Field, FormikHelpers } from "formik";
+import validator from "validator";
+interface MyFormValues {
+  imageURL: string;
+}
 
 const Month = ({
   imageURL,
@@ -34,10 +40,47 @@ const Month = ({
   month_name: string;
 }) => {
   const [hover, setHover] = useState(false);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
   const { user, extraInfo } = useAuth();
+  const initialValues: MyFormValues = { imageURL: "" };
 
   let height = ["250", "200"];
+
+  const validURLOptions = {
+    protocols: ["https"],
+    require_valid_protocol: true,
+    require_host: true,
+  };
+  async function validateURL(value: string) {
+    let error;
+    if (!value) {
+      error = "URL is required";
+    } else if (validator.isURL(value, validURLOptions)) {
+      try {
+        const res = await fetch(value);
+        const type = res.headers.get("Content-Type");
+        if (type !== "image/jpeg") {
+          return (error = "URL is not a direct link to an image");
+        }
+      } catch (e) {
+        error = "URL is not a direct link to an image";
+      }
+    } else {
+      error = "invalid URL";
+    }
+
+    return error;
+  }
+  function handleSubmit(
+    values: MyFormValues,
+    actions: FormikHelpers<MyFormValues>
+  ) {
+    console.log(values);
+    alert(JSON.stringify(values, null, 2));
+    actions.setSubmitting(false);
+    onToggle();
+  }
+
   return imageURL == "" ? (
     <Box
       onMouseEnter={() => setHover(true)}
@@ -75,48 +118,88 @@ const Month = ({
           {user && extraInfo ? (
             extraInfo.moderator ? (
               <>
-                <ModalBody>
-                  <FormControl id="imageURL">
-                    <FormLabel>
-                      Image URL
-                      <Popover placement="right">
-                        <PopoverTrigger>
-                          <IconButton
-                            icon={<FiHelpCircle />}
-                            aria-label="Help"
-                            boxSize={8}
-                            variant={"ghost"}
-                          />
-                        </PopoverTrigger>
-                        <PopoverContent>
-                          <PopoverBody>
-                            <Text fontWeight={"normal"}>
-                              The way this works is you upload an image on a
-                              free image hosting of your choosing (my favs :{" "}
-                              <Link
-                                href="https://postimages.org"
-                                color={"primary"}
-                              >
-                                postimages.org
-                              </Link>{" "}
-                              and{" "}
-                              <Link href="https://imgbb.com" color={"primary"}>
-                                imgbb.com
-                              </Link>{" "}
-                              ) and copy and paste the direct link here!
-                            </Text>
-                          </PopoverBody>
-                        </PopoverContent>
-                      </Popover>
-                    </FormLabel>
-                    <Input type="url" />
-                  </FormControl>
-                </ModalBody>
-                <ModalFooter>
-                  <Button variant="solid" bg="primary" color="white">
-                    Save
-                  </Button>
-                </ModalFooter>
+                <Formik
+                  initialValues={initialValues}
+                  onSubmit={(values, actions) => {
+                    handleSubmit(values, actions);
+                  }}
+                >
+                  {(props) => (
+                    <Form>
+                      <ModalBody>
+                        <Field name="imageURL" validate={validateURL}>
+                          {/*TODO: remove explicit any types */}
+                          {({ field, form }: { field: any; form: any }) => (
+                            <FormControl
+                              id="imageURL"
+                              isInvalid={
+                                form.errors.imageURL && form.touched.imageURL
+                              }
+                              isRequired
+                            >
+                              <FormLabel htmlFor="imageURL">
+                                Image URL
+                                <Popover placement="right">
+                                  <PopoverTrigger>
+                                    <IconButton
+                                      icon={<FiHelpCircle />}
+                                      aria-label="Help"
+                                      boxSize={8}
+                                      variant={"ghost"}
+                                    />
+                                  </PopoverTrigger>
+                                  <PopoverContent>
+                                    <PopoverBody>
+                                      <Text fontWeight={"normal"}>
+                                        The way this works is you upload an
+                                        image on a free image hosting of your
+                                        choosing (my favs :{" "}
+                                        <Link
+                                          href="https://postimages.org"
+                                          color={"primary"}
+                                        >
+                                          postimages.org
+                                        </Link>{" "}
+                                        and{" "}
+                                        <Link
+                                          href="https://imgbb.com"
+                                          color={"primary"}
+                                        >
+                                          imgbb.com
+                                        </Link>{" "}
+                                        ) and copy and paste the direct link
+                                        here!
+                                      </Text>
+                                    </PopoverBody>
+                                  </PopoverContent>
+                                </Popover>
+                              </FormLabel>
+                              <Input
+                                {...field}
+                                id="imageURL"
+                                placeholder="https://"
+                              />
+                              <FormErrorMessage>
+                                {form.errors.imageURL}
+                              </FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button
+                          variant="solid"
+                          bg="primary"
+                          color="white"
+                          type="submit"
+                          isLoading={props.isSubmitting}
+                        >
+                          Save
+                        </Button>
+                      </ModalFooter>
+                    </Form>
+                  )}
+                </Formik>
               </>
             ) : (
               <ModalBody>
