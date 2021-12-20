959daac5c8b5ffd09123d3f2eb9a4fd73c83725e
Tebogo Nomnqa <tebzax2@gmail.com>
2021-12-20 13:25:27 +0200

    feat: upload screenshot to Firebase storage

--- takeNewScreenshot.js
+++ takeNewScreenshot.js
@@ -1,13 +1,20 @@
 const puppeteer = require('puppeteer');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getStorage } = require('firebase-admin/storage');
 
-(async () => {
+const serviceAccount = require('./firebase/serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: 'sa-trends-calendar.appspot.com'
+});
+
+async function takeScreenshot() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({ width: 1440, height: 1025 });
     await page.goto('http://localhost:3000/en/year/2021');
     await page.evaluate(() => {
-
-
         let logo = document.querySelector('.css-70qvj9');
         let madeWithText = document.getElementsByClassName('css-70qvj9')[2];
         let copyrightText = document.querySelector('.css-zm3ud3');
@@ -22,10 +29,27 @@ const puppeteer = require('puppeteer');
         madeWithText.parentNode.removeChild(madeWithText);  
         
         copyrightText.innerHTML = "fresh copy of the calendar downloaded at https://sacalendar.vercel.app";
-        
-    
     });
-    await page.screenshot({ path: 'screenshot.jpg',clip: { x: 0, y: 50, width: 1440, height: 937 } });
-    await browser.close();
+   await page.screenshot({ path: 'screenshot.jpg', clip: { x: 0, y: 50, width: 1440, height: 937 } });
+   await browser.close();
+   console.log('screenshot captured successfully');
+}
 
-})();
+async function uploadImage() {
+  try {
+     const bucket = getStorage().bucket();
+     await bucket.upload("screenshot.jpg", {
+        destination: "calendar-screenshots/2021.jpg",
+    });
+  console.log('screenshot successfully uploaded!');
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+async function run() {
+  await takeScreenshot();
+  await uploadImage();
+}
+
+run();
