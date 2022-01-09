e336751c41a9f555a57e887ad3eda76c2d7a0397
Tebogo Nomnqa <tebzax2@gmail.com>
2022-01-09 21:42:41 +0200

    fix: unecessary modifications to webpage before taking a screenshot

--- takeNewScreenshot.js
+++ takeNewScreenshot.js
@@ -21,23 +21,16 @@ async function takeScreenshot() {
   });
     await page.evaluate(() => {
         let logo = document.querySelector('.css-70qvj9');
-        let madeWithText = document.getElementsByClassName('css-70qvj9')[2];
-        let copyrightText = document.querySelector('.css-zm3ud3');
         let nav = document.querySelector('.css-ajfj5u');
         let NavBtns = document.querySelectorAll('.css-1m9bns0');
-      
         nav.replaceChild(logo, nav.childNodes[0]);
- 
         for (i = 0; i < NavBtns.length; i++){
             NavBtns[i].parentNode.removeChild(NavBtns[i]);
         }
-        madeWithText.parentNode.removeChild(madeWithText);  
-        
-        copyrightText.innerHTML = "fresh copy of the calendar downloaded at https://sacalendar.vercel.app";
     });
-   await page.screenshot({ path: `${year}-screenshot.jpg`, clip: { x: 0, y: 60, width:992, height: 800 } });
-   await browser.close();
-   console.log('screenshot captured successfully');
+    await page.screenshot({ path: `${year}-screenshot.jpg`, clip: { x: 0, y: 60, width:992, height: 800 } });
+    await browser.close();
+    console.log('screenshot captured successfully');
 }
 
 async function uploadImage() {
